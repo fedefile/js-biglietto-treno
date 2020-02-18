86752e32d1e423f750028939cb50b38e19b3ccf6
federico filella <federico.filella@gmail.com>
2020-02-18 22:34:46 +0100

    setup

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere
+e l’età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+Il prezzo del biglietto è definito in base ai km (0.21 € al km),
+ ma va applicato uno sconto del 20% per i minorenni
+ e del 40% per gli over 65. */
+
+ // dimmi i kilometri
+ // dimmi l'età
+ // restituisco l'importo che spenderai per viaggiare
+
+
+  // 0.21 € al km
+  // -20% under 18
+  // -40% over 65
