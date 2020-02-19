bba87156f33a53c76777a511f21432d6994f4cd6
federico filella <federico.filella@gmail.com>
2020-02-19 20:10:25 +0100

    setup post

--- js/main.js
+++ js/main.js
@@ -18,21 +18,32 @@ Il prezzo del biglietto è definito in base ai km (0.21 € al km),
   // -20% under 18
   // -40% over 65
 
-var etaAnagrafica = prompt("Qual'è la tua età?")
-console.log(etaAnagrafica);
-var distanza = prompt("Quanti km vuoi percorrere?")
-console.log(distanza)
-if (etaAnagrafica > 65) {
-    console.log('-40% sconto over 65');
-} else if (etaAnagrafica < 18) {
-    console.log('-20% sconto under 18');
-} else {
-    console.log('tariffa standard adulto');
-}
+  var maggiorEta = 18;
+  var anziano = 65;
+  var prezzoAlKm = 0.21;
+  var scontoMinorenne = 20;
+  var scontoAnziano = 40;
 
-
-
-
-
-
-getElementById('container').setAttribute("class","invisible")
+  var km = parseInt(prompt("Quanti km devi percorrere?"));
+  console.log(km);
+  if (!isNan(km)) {
+      var eta = parseInt(prompt("Quanti anni hai?"));
+      if (!isNaN(eta)){
+          var tariffaBiglietto = prezzoAlKm * km;
+          console.log("Prezzo al biglietto standard: " + tariffaBiglietto.toFixed(2) + "€");
+          document.getElementById('prezzo-standard').innerHTML = tariffaBiglietto.toFixed(2);
+          var tariffaScontataGiovani = tariffaBiglietto * (1- scontoMinorenne/100);
+          console.log("Prezzo biglietto giovani: " + tariffaScontataGiovani.toFixed(2) + "€");
+          document.getElementById('prezzo-scontato').innerHTML = tariffaBiglietto.toFixed(2);
+            }
+        else if(eta >= anziano) {
+        var tariffaScontataAnziani = tariffaBiglietto * (1 - scontoAnziano/100);
+        console.log("Prezzo biglietto anziani: " + tariffaScontataAnziani.toFixed(2) + "€");
+        document.getElementById('prezzo-scontato').innerHTML = tariffaBiglietto.toFixed(2);
+    }
+        } else {
+            console.log("inserire numero");
+        }
+        } else {
+        console.log("scrivi un numero");
+        }
